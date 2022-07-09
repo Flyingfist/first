bf6d48e9a1ab5499cca902aad8a5b1ff0fc30d84
Juri <inoshishi@yandex.ru>
2022-07-09 16:54:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("Are you already eighteen?","18");
+console.log(typeof(answer));
